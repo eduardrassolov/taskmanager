606617a19c05b327e0eb16f52648372612415d80
eduardrassolov <eduard.rassolov@outlook.com>
2023-07-21 09:03:15 +0300

    -_-

--- src/components/navbar/Logo.jsx
+++ src/components/navbar/Logo.jsx
@@ -5,7 +5,7 @@ function Logo() {
     <>
       <div className="m-2 flex items-center">
         <NavLink to="/" className="flex">
-          <img src="../../public/logo.png" className="w-8" alt="logo" />
+          <img src="../../src/img/logo.png" className="w-8" alt="logo" />
 
           <p className="mx-1 hidden text-xl font-semibold text-gray-800 sm:block">
             TrackMate
